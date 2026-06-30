17adaf168ec169595c236afc9c136eb710912c30
Alvaro Grijalva <algm522rx@gmail.com>
2026-06-30 02:36:23 -0700

    Updated backend

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,9 @@
 require("dotenv").config();
-const express = require("express");
-const cors = require("cors");
-const nodemailer = require("nodemailer");
-const validateQuoteForm = require("./middleware/validateQuoteForm");
-const app = express();
+const express=require("express");
+const cors=require("cors");
+const nodemailer=require("nodemailer");
+const validateQuoteForm=require("./middleware/validateQuoteForm");
+const app=express();
 
 app.use(cors());
 app.use(express.json());
